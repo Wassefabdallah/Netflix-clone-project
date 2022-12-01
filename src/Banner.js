2f8e788087b8b708a98ac33fe9fed8e959c384d4
Wassefabdallah <wassefabdallah99@gmail.com>
2022-12-01 03:06:43 +0100

    first commit

---
+++ src/Banner.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import "./Banner.css";
+import requests from "./Request";
+import axios from './axios';
+
+
+function Banner() {
+  //FETCH THE MOVIE FROM TMDB
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
+  //ADD PONCTIATION AT THE END OF A LONG STRING
+  function truncate(string, length) {
+    if (string?.length > length) return string.substring(0, length + 2) + "...";
+    else return string;
+  }
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.orignal_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 200)}
+        </h1>
+      </div>
+
+      <div className="banner--fadeBottom" />
+    </header>
+  );
+}
+
+export default Banner;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCX_tbgGQss68QwO1iBE3UtvHVro_ByhDo",
+    authDomain: "netflix-clone-36810.firebaseapp.com",
+    projectId: "netflix-clone-36810",
+    storageBucket: "netflix-clone-36810.appspot.com",
+    messagingSenderId: "437996977467",
+    appId: "1:437996977467:web:b4cc62df930a99dab30b1e"
+  };
